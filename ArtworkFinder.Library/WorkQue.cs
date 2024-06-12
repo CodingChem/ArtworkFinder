c26047ffacaeefbc092f24b432c639538e3cf0b6
Vegard Pareli Seines <Vegard.Seines@outlook.com>
2024-06-12 15:09:10 +0200

    Implemented a que for storing work in progress.

---
+++ ArtworkFinder.Library/WorkQue.cs
@@ -0,0 +1,40 @@
+﻿using ArtworkFinder.Library.Exceptions;
+using ArtworkFinder.Library.Models;
+using System.Collections.Concurrent;
+
+namespace ArtworkFinder.Library;
+
+public class WorkQue
+{
+	private BlockingCollection<Action> _taskQue = new BlockingCollection<Action>();
+	public WorkQue()
+	{
+		// Start the task processor
+		Task.Run(() => ProcessQueue());
+	}
+
+	public void EnqueueTask(Action task)
+	{
+		if (task == null) throw new ArgumentNullException(nameof(task));
+		_taskQue.Add(task);
+	}
+
+	private void ProcessQueue()
+	{
+		foreach (var task in _taskQue.GetConsumingEnumerable())
+		{
+			try
+			{
+				task.Invoke();
+			}
+			catch (CustomerNotFoundException e)
+			{ 
+				Console.WriteLine(e.Message + " " + e.CustomerName);
+			}
+		}
+	}
+	public void Finish()
+	{
+		_taskQue.CompleteAdding();
+	}
+}
