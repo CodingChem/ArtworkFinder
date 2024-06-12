fbe78b45da19d0fe2d4b16855df242a4d96e12db
Vegard Pareli Seines <Vegard.Seines@outlook.com>
2024-06-12 09:59:00 +0200

    Add project files.

---
+++ ArtworkFinder.Library/Models/ArtworkDataModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtworkFinder.Library.Models
+{
+	public class ArtworkDataModel
+	{
+        public string Path { get; set; }
+        public string Name { get { return Path.Split('/')[Path.Split('/').Length - 1]; } }
+        public bool IsArchived { get { return Path.Contains("rchi"); } }
+        public DateTime LastModified { get { return File.GetLastWriteTime(Path); } }
+    }
+}
---
+++ ArtworkFinder.Library/Models/FpackDataModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ArtworkFinder.Library.Models
+{
+    public class FpackDataModel
+    {
+        public string CustomerName { get; set; }
+        public string ItemNumber { get; set; }
+        public List<ArtworkDataModel> ArtworkFiles { get; set; }
+
+    }
+}
