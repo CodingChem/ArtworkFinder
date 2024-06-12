9df087523d38ceb6a2090e8d8518aaf20838ed2f
Vegard Pareli Seines <Vegard.Seines@outlook.com>
2024-06-12 14:55:48 +0200

    Cleaned up whitespace

--- ArtworkFinder.Library/Models/ArtworkDataModel.cs
+++ ArtworkFinder.Library/Models/ArtworkDataModel.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ArtworkFinder.Library.Models
+﻿namespace ArtworkFinder.Library.Models;
+public class ArtworkDataModel
 {
-	public class ArtworkDataModel
+	public ArtworkDataModel(string path)
 	{
-        public string Path { get; set; }
-        public string Name { get { return Path.Split('/')[Path.Split('/').Length - 1]; } }
-        public bool IsArchived { get { return Path.Contains("rchi"); } }
-        public DateTime LastModified { get { return File.GetLastWriteTime(Path); } }
-    }
+		Path = path;
+	}
+
+	public string Path { get; set; }
+    public string Name { get { return Path.Split('/')[Path.Split('/').Length - 1]; } }
+    public bool IsArchived { get { return Path.Contains("rchi"); } }
+    public DateTime LastModified { get { return File.GetLastWriteTime(Path); } }
 }
--- ArtworkFinder.Library/Models/FpackDataModel.cs
+++ ArtworkFinder.Library/Models/FpackDataModel.cs
@@ -1,10 +1,16 @@
-﻿namespace ArtworkFinder.Library.Models
+﻿namespace ArtworkFinder.Library.Models;
+
+public class FpackDataModel
 {
-    public class FpackDataModel
-    {
-        public string CustomerName { get; set; }
+	public FpackDataModel(string customerName, string itemNumber)
+	{
+		CustomerName = customerName ?? throw new ArgumentNullException(nameof(customerName));
+		ItemNumber = itemNumber ?? throw new ArgumentNullException(nameof(itemNumber));
+		ArtworkFiles = new();
+	}
+
+	public string CustomerName { get; set; }
         public string ItemNumber { get; set; }
         public List<ArtworkDataModel> ArtworkFiles { get; set; }
 
-    }
 }
