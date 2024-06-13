363c82539012d6da92e9a38cb81cbe12a42c9a77
Vegard Pareli Seines <Vegard.Seines@outlook.com>
2024-06-13 14:27:57 +0200

    Rewrote Library entrypoint.
    Moved to an event based model.
    removed depriciated exception.
    
    Added ArtworkFinder class as a entry point to this library. Removed old FpackList class as it was a mess.
    
    Added Fpack class to manage searching for its own artwork.

---
+++ ArtworkFinder.Library/Events/SearchCompletedEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtworkFinder.Library.Events;
+
+internal class SearchCompletedEventArgs : EventArgs
+{
+    public bool ArtworkFound { get; private set; }
+    public string Fpack { get; private set; }
+    public SearchCompletedEventArgs(bool artworkFound, string fpack)
+    {
+        ArtworkFound = artworkFound;
+        Fpack = fpack;
+    }
+}
---
+++ ArtworkFinder.Library/Fpack.cs
@@ -0,0 +1,61 @@
+﻿using ArtworkFinder.Library.Events;
+using ArtworkFinder.Library.Models;
+namespace ArtworkFinder.Library;
+
+internal class Fpack
+{
+    internal event EventHandler<SearchCompletedEventArgs>? SearchCompletedEvent;
+    public string CustomerName { get; private set; }
+    public string ItemNumber { get; private set; }
+    public bool IsSearching { get; private set; }
+    public List<ArtworkDataModel> ArtworkFiles { get; private set; }
+
+    public Fpack(string customerName, string itemNumber)
+    {
+        CustomerName = customerName ?? throw new ArgumentNullException(nameof(customerName));
+        ItemNumber = itemNumber ?? throw new ArgumentNullException(nameof(itemNumber));
+        IsSearching = false;
+        ArtworkFiles = new();
+    }
+    internal void RunSearch(string[] customerDirectories)
+    {
+        IsSearching = true;
+        var customerDirectory = customerDirectories.FirstOrDefault(x => x.Contains(CustomerName, StringComparison.OrdinalIgnoreCase));
+        if (customerDirectory == null)
+        {
+            IsSearching = false;
+            SearchCompletedEvent?.Invoke(this, new SearchCompletedEventArgs(false, ToString()));
+            return;
+        }
+        var itemDirectories = Directory.GetDirectories(customerDirectory, "Item");
+        if (itemDirectories.Length == 0)
+        {
+            foreach (var dir in Directory.GetDirectories(customerDirectory))
+            {
+                var itemDirectory = Directory.GetDirectories(dir, "Item");
+                if (itemDirectory.Length != 0)
+                    itemDirectories = itemDirectories.Concat(itemDirectory).ToArray();
+            }
+        }
+        foreach (var item_dir in itemDirectories)
+        {
+            var paths = Directory.GetFiles(item_dir, "*" + ItemNumber + "*", SearchOption.AllDirectories);
+            foreach (var path in paths)
+            {
+                ArtworkFiles.Add(
+                    new ArtworkDataModel(path)
+                );
+            }
+        }
+        IsSearching = false;
+        SearchCompletedEvent?.Invoke(this, new SearchCompletedEventArgs(ArtworkFiles.Count > 0, ToString()));
+    }
+    public override string ToString()
+    {
+        return $"{CustomerName}: {ItemNumber}";
+    }
+    public FpackDataModel ToFpackDataModel()
+    {
+        return new FpackDataModel(CustomerName, ItemNumber, IsSearching ? null : ArtworkFiles);
+    }
+}
